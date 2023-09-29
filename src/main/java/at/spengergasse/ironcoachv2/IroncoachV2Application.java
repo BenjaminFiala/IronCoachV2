d83769a8a236c83dd799f4cbf3cc26e9a5af739c
benjamin <fia22503spengergasse.at>
2023-09-29 09:26:07 +0200

    Initial commit

---
+++ src/main/java/at/spengergasse/ironcoachv2/IroncoachV2Application.java
@@ -0,0 +1,13 @@
+package at.spengergasse.ironcoachv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroncoachV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IroncoachV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/at/spengergasse/ironcoachv2/IroncoachV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.ironcoachv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IroncoachV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
